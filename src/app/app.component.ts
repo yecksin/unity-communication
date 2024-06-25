d348c098c17d760214cfe6b7f700ad6559c9aed2
yecksin <yecksin@gmail.com>
2024-06-24 23:03:19 -0500

    sen event from unity to angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -22,4 +22,31 @@ export class AppComponent {
       productVersion: "1.0"
     });
 }
+
+constructor(private zone: NgZone) {
+  // Asignar la referencia de Angular a una variable global
+  window['angularComponentRef'] = {
+    component: this,
+    zone: this.zone
+  };
+}
+
+// Método para manejar la inicialización de Unity
+onUnityReady(message: string) {
+  console.log('Unity Ready:', message);
+  // Aquí puedes manejar lo que ocurra cuando Unity esté listo
+}
+
+// Método para manejar eventos desde Unity
+onUnityEvent(message: string) {
+  console.log('Event from Unity:', message);
+  // Aquí puedes manejar lo que ocurra con el evento de Unity
+}
+
+onCube(message: string) {
+  console.log('Event from Unity:', message);
+  console.log("cube clicked");
+  // Aquí puedes manejar lo que ocurra con el evento de Unity
+}
+
 }
